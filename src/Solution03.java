41e1d8633fa5525bbbf75b758b2bdeec534dbd04
YOLO-NIM <kjyy28@gmail.com>
2026-06-24 15:06:14 +0900

    feat: refine Java example implementations

--- src/Solution03.java
+++ src/Solution03.java
@@ -1,5 +1,39 @@
+// 업캐스팅, 다운캐스팅
 public class Solution03 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Animal a = new Dog();  // 묵시적, 암시적(upcasting) -> 상속받거나 구현한 상위 클래스/인터페이스로 대입
+        a.eat();
+        Dog d = (Dog) a; // 힙 메모리 상에서는 a -> Dog와 인접하기 때문에 바로 사용 가능 -> 명시적 변환
+        d.eat(); // 명시적으로 해야한다는 것 외에는 문제 X.
+        // 다운캐스팅.
+        Animal a2 = new Animal();
+        // Exception in thread "main" java.lang.ClassCastException: class Animal cannot be cast to class Dog (Animal and Dog are in unnamed module of loader 'app')
+//        Dog d2 = (Dog) a2; // classCastException
+//        d2.eat();
+        if (a2 instanceof Dog) {
+            System.out.println("a2 is Dog");
+            Dog d2 = (Dog) a2; // 상대적으로 안전한 방식
+            d2.eat();
+        } else {
+            System.out.println("a2 is not Dog");
+        }
+
+        if (a instanceof Dog dd) { // 패턴매칭 축
+            dd.eat(); // jdk 16++
+        }
+
+    }
+}
+
+class Animal {
+    void eat() {
+        System.out.println("Animal.eat");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void eat() {
+        System.out.println("Dog.eat");
     }
 }
--- src/Solution05.java
+++ src/Solution05.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
-
 public class Solution05 {
+    public static void main(String[] args) {
+        TV tv = new TV();
+        Machine m = tv;
+        m.drive();
+//        m.watch();
+        Video v = tv;
+        v.watch();
+//        v.drive();
+    }
+
+    static void useMachine(Machine n) {
+        n.drive();
+    }
+
+    static void useMachine(Video v) {
+        v.watch();
+    }
+}
+
+// 1. 한 클래스를 여러 타입의 하위 클래스로 만들고 싶음 (패러미터, 변수)
+// 2. 한 클래스가 여러 구현 계약을 수행하게 하고 싶음 (오버라이드 메서드)
+//abstract class Machine {
+interface Machine {
+    //    abstract void work();
+    //    void work();
+    // 인터페이스는 다중 구현이 가능하다.
+    void drive();
+}
+
+interface Video {
+    //    abstract void work();
+    //    void work();
+    void watch();
+}
+
+// 인터페이스는 힙 영역 접근 X (멤버변수 X. 오로지 같은 인터페이스 내에 구현 요청된 것들만 같이 쓸 수 있음)
+
+// Class cannot extend multiple classes
+//class TV extends Machine, Video {}
+class TV implements Machine, Video {
+    @Override
+    public void drive() {
+        System.out.println("TV.drive");
+    }
+
+    @Override
+    public void watch() {
+        System.out.println("TV.watch");
+    }
 }
